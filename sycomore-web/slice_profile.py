673ac19b775479dd9ff0a3228136eb60f1cad214
Julien Lamy <lamy@unistra.fr>
2019-10-19 14:15:35 +0200

    Use 3D EPG for slice thickness

--- sycomore-web/slice_profile.py
+++ sycomore-web/slice_profile.py
@@ -9,8 +9,8 @@ name = "Slice profile"
 figure = bqplot.pyplot.figure(
     legend_location="top-right", 
     fig_margin={"top": 0, "bottom": 30, "left": 60, "right": 20})
-transversal_plot = bqplot.pyplot.plot([], [], "-b", labels=["M∥"])
-longitudinal_plot = bqplot.pyplot.plot([], [], "-g", labels=["M⟂"])
+transversal_plot = bqplot.pyplot.plot([], [], "-b", labels=["M⟂"])
+longitudinal_plot = bqplot.pyplot.plot([], [], "-g", labels=["M∥"])
 bqplot.pyplot.xlabel("Position (mm)")
 bqplot.pyplot.ylabel("Magnitude")
 bqplot.pyplot.legend()
@@ -18,7 +18,6 @@ bqplot.pyplot.legend()
 def update_plot(change):
     slice_thickness = 1*mm
     pulse_support_size = 101
-    sampling_support_size = 501
 
     t0 = pulse_duration.value*ms/(2*zero_crossings.value)
 
@@ -36,26 +35,44 @@ def update_plot(change):
         /sinc_pulse.get_time_interval().duration)
     
     species = sycomore.Species(T1.value*ms, T2.value*ms)
-    model = sycomore.epg.Regular(species)
-    for hard_pulse in sinc_pulse.get_pulses():
-        model.apply_pulse(hard_pulse.angle, hard_pulse.phase)
-        model.apply_time_interval(gradient_duration, gradient_amplitude)
-
-    # Unfold the F and the Z states
-    F = numpy.hstack((model.states[:,0], numpy.conj(model.states[1:,1][::-1])))
-    Z = numpy.hstack((model.states[:,2], model.states[1:,2][::-1]))
-
-    # Perform iFFT, and shift it since the spatial axis must be centered on zero.
-    M_transversal = numpy.fft.fftshift(numpy.fft.ifft(F))
-    M_longitudinal = numpy.fft.fftshift(numpy.fft.ifft(Z))
     
-    # NOTE: with 1-D EPG, the slice-selection gradient is played on the first/only 
-    # axis, which is also the readout axis. The roles of transversal and 
-    # longitudinal are thus flipped (cf. the labels of the plots)
+    model = sycomore.epg.Discrete3D(species)
+    for index, hard_pulse in enumerate(sinc_pulse.get_pulses()):
+        model.apply_pulse(hard_pulse.angle, hard_pulse.phase)
+        model.apply_time_interval(
+            gradient_duration, [0*T/m, 0*T/m, gradient_amplitude])
+
+    # Unfold the F and the Z states: create an array for all orders, including
+    # empty ones
+    orders = numpy.reshape(model.orders, (-1,3))[:,2]
+    orders = [int(x.magnitude) for x in orders / model.bin_width]
+    F = numpy.zeros(2*max(orders)+1, model.states.dtype)
+    Z = numpy.zeros(2*max(orders)+1, model.states.dtype)
+    for order, state in zip(orders, model.states):
+        F[order] = state[0]
+        Z[order] = state[2]
+        
+        if order != 0:
+            F[-order] = state[1].conj()
+            Z[-order] = state[2]
+    
+    # Perform iFFT, and shift it since the spatial axis must be centered on zero.
+    M_transversal = numpy.fft.fftshift(numpy.fft.ifft(F, norm="ortho"))
+    M_longitudinal = numpy.fft.fftshift(numpy.fft.ifft(Z, norm="ortho"))
+    
     x_axis = numpy.asarray([
         (2*numpy.pi*x/sinc_pulse.get_gradient_moment()[2]).convert_to(mm) 
         for x in range(len(M_transversal))])
     x_axis -= 0.5*(x_axis[0]+x_axis[-1])
+    
+    # Crop between [-slice_thickness, +slice_thickness]
+    slice_ = (
+        numpy.searchsorted(x_axis, -slice_thickness.convert_to(mm), "left"),
+        numpy.searchsorted(x_axis, +slice_thickness.convert_to(mm), "right"),
+    )
+    x_axis = x_axis[slice_[0]:slice_[1]]
+    M_transversal = M_transversal[slice_[0]:slice_[1]]
+    M_longitudinal = M_longitudinal[slice_[0]:slice_[1]]
     
     transversal_plot.x = x_axis
     transversal_plot.y = numpy.abs(M_transversal)
@@ -63,7 +80,7 @@ def update_plot(change):
     longitudinal_plot.x = x_axis
     longitudinal_plot.y = numpy.abs(M_longitudinal)
     
-    bqplot.pyplot.xlim(-slice_thickness.convert_to(mm), slice_thickness.convert_to(mm))
+    bqplot.pyplot.xlim(x_axis[0], x_axis[-1])
 
 species_label = ipywidgets.widgets.HTML(value="""<div style="text-align:center; font-size: larger">Species</div>""")
 T1 = ipywidgets.widgets.BoundedIntText(
